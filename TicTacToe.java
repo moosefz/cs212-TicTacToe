31f39af44661ac06cb55833fccf6b0049a14e6f1
Moose Fawaz <moosefawaz@gmail.com>
2020-04-27 13:36:26 -0400

    TicTacToe consists of main and creates a new game once executed.

---
+++ TicTacToe.java
@@ -0,0 +1,15 @@
+/**
+ * @author Mustafa Fawaz, ID: 103184737
+ * @version 1
+ */
+
+/**
+ * TicTacToe is a class that consists of the main for the functionality of the game.
+ */
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        Game myGame = new Game();
+        myGame.start();
+    }
+}
