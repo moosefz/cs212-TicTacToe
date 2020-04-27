988655166e8af447844901865959eed8c1973967
Moose Fawaz <moosefawaz@gmail.com>
2020-04-27 13:34:58 -0400

    Global is an interface that consists of constants used.

---
+++ Global.java
@@ -0,0 +1,18 @@
+/**
+ * @author Mustafa Fawaz, ID: 103184737
+ * @version 1
+ */
+
+/**
+ * Global is an interface that is implemented by any class to maintain shared constants such as empty block, game draw,
+ * playerX and playerO as integers for tracking.
+ */
+
+public interface Global {
+
+    public static final int DRAW = 3;
+    public static final int EMPTY = 0;
+    public static final int PLAYERX = 1;
+    public static final int PLAYERO = 2;
+
+}
